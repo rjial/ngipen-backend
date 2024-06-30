c67a63a5bc6c947b82861a6b251d521c425c9778
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2024-07-01 02:12:08 +0700

    fixed #1

--- src/main/java/com/rjial/ngipen/common/HttpMediaTypeNotAcceptableExceptionHandler.java
+++ src/main/java/com/rjial/ngipen/common/HttpMediaTypeNotAcceptableExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 @Slf4j
 public class HttpMediaTypeNotAcceptableExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
